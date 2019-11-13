ceefd189361aed4695d2004eb57863fe96e4ee0a
Luis <luis.stauss@gmx.de>
2019-11-13 18:36:44 +0100

    initial commit

---
+++ Assets/Scripts/Controll.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D rb2d;
+    public float moveSpeed = 5;
+    public Camera cam;
+
+    Vector2 movement;
+    Vector2 mousePos;
+    
+   
+    void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + movement * moveSpeed * Time.fixedDeltaTime);
+        Vector2 direction = mousePos - rb2d.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 10f;
+        rb2d.rotation = angle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform firePoint;
+    public int damage = 10;
+    public LineRenderer lineRenderer;
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            
+            StartCoroutine(Shoot());
+        }
+        
+    }
+    IEnumerator Shoot()
+    {
+        
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+        if (hitInfo)
+        {
+            Debug.Log(hitInfo);
+            Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, hitInfo.point);
+        }
+        else
+        {
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, firePoint.position + firePoint.right * 100);
+        }
+        lineRenderer.enabled = true;
+        yield return new WaitForSeconds(0.02f);
+        lineRenderer.enabled = false;
+    }
+}
